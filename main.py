991d348943eb80af91dd8c64db1da1e90382e3da
yuritorresf <feereira@gmail.com>
2022-08-02 17:24:43 -0300

    Creating a GUI for the software

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-from genericpath import isfile
-import os
+from PyQt6 import uic, QtWidgets
+from PyQt6.QtWidgets import QApplication, QWidget
+import os, sys
+import time
 
 class Organizer:
 
     def __init__(self):
-        self.__folder_loc = input('Enter folder location: ')
+        # self.__folder_loc = input('Enter folder location: ')
         self.__folders = []
         self.__type = []
-    
+
+        self.app = QtWidgets.QApplication(sys.argv)
+        self.window = uic.loadUi('file_organizer.ui')
+
+        self.window.show()
+
+        self.window.search_button.clicked.connect(self.action)
+
+        self.app.exec()
+        
+    def action(self):
+        path = str(QtWidgets.QFileDialog.getExistingDirectory())
+        self.window.input_text.setText(path)
+        for file in os.listdir(path):
+            self.window.list_filesfolders.addItem(str(file))
+
     def files(self):
         for file in os.listdir(self.__folder_loc):
             if os.path.isfile(os.path.join(self.__folder_loc, file)):
@@ -25,20 +42,26 @@ class Organizer:
                 print('Created folder: ' + file)
 
     def move_files(self):
+        d=0
         for file in os.listdir(self.__folder_loc):
             if os.path.isfile(os.path.join(self.__folder_loc, file)):
                 if file.split('.')[-1] in self.__type:
                     os.rename(os.path.join(self.__folder_loc, file), os.path.join(self.__folder_loc, file.split('.')[-1], file))
                     print('Moved file: ' + file)
+                    d+=1
+                    print(f'{d/100}')
+
 
 def main():
     organizer = Organizer()
-    organizer.files()
-    organizer.create_files()
-    organizer.move_files()
-    print('Done!')
+    #organizer.files()
+    #organizer.create_files()
+    #organizer.move_files()
+    #print('Done!')
     input('Press enter to exit...')
-    exit()
+    #exit()
+    
+    # os.listdir("C:/Users/ASCOM/").__len__()
 
 if __name__ == '__main__':
     main()
