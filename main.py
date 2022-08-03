b8f6e7242eeb4481c5707ef31fa7d56a0e166660
yuritorresf <feereira@gmail.com>
2022-08-03 15:58:42 -0300

    formating extensions

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ class Organizer:
     def files(self):
         for file in os.listdir(self.__folder_loc):
             if os.path.isfile(os.path.join(self.__folder_loc, file)):
-                if file.split('.')[-1] not in self.__type:
+                if file.split('.')[-1].lower() not in self.__type:
                     self.__type.append(file.split('.')[-1])
 
             if not os.path.isfile(os.path.join(self.__folder_loc, file)):
@@ -85,7 +85,7 @@ class Organizer:
 
         for file in os.listdir(self.__folder_loc):
             if os.path.isfile(os.path.join(self.__folder_loc, file)):
-                if file.split('.')[-1] in self.__type:
+                if file.split('.')[-1].lower() in self.__type:
                     self.__counter_actions += 1
         
         dprint+=f"\n{len(os.listdir(self.__folder_loc))} arquivos encontrados.\n"
@@ -109,7 +109,7 @@ class Organizer:
         
         for file in os.listdir(self.__folder_loc):
             if os.path.isfile(os.path.join(self.__folder_loc, file)):
-                if file.split('.')[-1] in self.__type:
+                if file.split('.')[-1].lower() in self.__type:
                     os.rename(os.path.join(self.__folder_loc, file), os.path.join(self.__folder_loc, file.split('.')[-1], file))
                     d+=1
                     data = (d/self.__counter_actions)*100
